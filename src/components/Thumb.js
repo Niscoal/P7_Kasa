f72a0bdda534eb81bb13a730b4c7e1ca33153baa
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-29 20:55:58 +0100

    props changes

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Thumb = ({ props }) => {
+const Thumb = (props) => {
     return (
         <div>
-            <Link to={`/logement/${props.id}`}>
-                <img src={props.cover} alt="Illustration"></img>
-                <h3>{props.title}</h3>
+            <Link to={`/logement/${props.details.id}`}>
+                <img src={props.details.cover} alt="Illustration"></img>
+                <h3>{props.details.title}</h3>
             </Link>
         </div>
     );
