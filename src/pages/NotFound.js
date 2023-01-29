75c06c106abc1f4be215883b907cf341a2348ce6
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-30 00:13:50 +0100

    NotFound page ended

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
         <div>
-            <p>404</p>
+            <h1>404</h1>
+            <h2>Oups ! La page que vous demandez n'existe pas</h2>
+            <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
     );
 };
