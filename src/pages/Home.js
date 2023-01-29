6bf1c454c28e85444fcb840331e052bfa9569511
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-30 00:17:49 +0100

    Home page : <div> to <main> tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import Banner from "../components/Banner.js";
 import BannerImg from "../assets/banner_img.png";
 import Logements from "../data/logements.json";
 import Thumb from "../components/Thumb.js";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner bannerImg={BannerImg} />
+    try {
+        return (
             <div>
-                {Logements.map((logement) => {
-                    return <Thumb key={logement.id} details={logement} />;
-                })}
+                <Banner bannerImg={BannerImg} alt="banner : forest and sea" />
+                <main>
+                    {Logements.map((logement) => {
+                        return <Thumb key={logement.id} details={logement} />;
+                    })}
+                </main>
             </div>
-        </div>
-    );
+        );
+    } catch {
+        return <Navigate to="/notFound"></Navigate>;
+    }
 };
 
 export default Home;
