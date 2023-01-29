229cd8914db7c48f5c0bdf5062f193ff74bfcc3c
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-30 00:18:26 +0100

    About page logic added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Banner from "../components/Banner";
+import about_banner_img from "../assets/about_banner_img.png";
+import Collapse from "../components/Collapse";
+import about from "../data/about.json";
 
 const About = () => {
-    return <div>A propos</div>;
+    return (
+        <div>
+            <Banner bannerImg={about_banner_img} alt="Banner : moutains" />
+            <main>
+                {about.map((worth, index) => {
+                    return (
+                        <Collapse
+                            key={index}
+                            collapseTitle={worth.title}
+                            collapseContent={worth.text}
+                        />
+                    );
+                })}
+            </main>
+        </div>
+    );
 };
 
 export default About;
