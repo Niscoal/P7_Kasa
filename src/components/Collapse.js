fdf3eeb59b754be71f20cb0a9dc14adf42de7763
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-29 21:44:28 +0100

    Collapse : usestate variables changes to avoid misunderstanding

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -3,19 +3,19 @@ import chevronDown from "../assets/collapse_chevronDown.png";
 import chevronUp from "../assets/collapse_chevronUp.png";
 
 const Collapse = (props) => {
-    const [open, close] = useState(false);
+    const [initial, setOpening] = useState(false);
     return (
         <div>
-            <div onClick={() => close(!open)}>
+            <div onClick={() => setOpening(!initial)}>
                 <div>
                     <span className="collapse__title">
                         {props.collapseTitle}
                     </span>
-                    {open && <img src={chevronDown} alt="chevron down" />}
-                    {!open && <img src={chevronUp} alt="chevron up" />}
+                    {initial && <img src={chevronDown} alt="chevron down" />}
+                    {!initial && <img src={chevronUp} alt="chevron up" />}
                 </div>
             </div>
-            {open && (
+            {initial && (
                 <div className="collapse__content">{props.collapseContent}</div>
             )}
         </div>
