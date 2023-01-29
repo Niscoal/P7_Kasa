1443932159a7eb407182c26a36fdabb2dd0a450f
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-01-30 00:15:32 +0100

    Logement page: try/catch added to fix 404 redirection issue

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,58 +1,60 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import logements from "../data/logements.json";
 import Carousel from "../components/Carousel.js";
 import Tags from "../components/Tags.js";
 import Collapse from "../components/Collapse.js";
 
 const Logement = () => {
-    let paramsId = useParams();
+    try {
+        let paramsId = useParams();
+        const logement = logements.find((theOne) => theOne.id === paramsId.id);
 
-    const logement = logements.find((theOne) => theOne.id === paramsId.id);
-    // utilisation de la destructuration:
-    const {
-        id,
-        title,
-        pictures,
-        description,
-        host,
-        rating,
-        location,
-        equipments,
-        tags,
-    } = logement;
+        // utilisation de la destructuration:
+        const {
+            id,
+            title,
+            pictures,
+            description,
+            host,
+            rating,
+            location,
+            equipments,
+            tags,
+        } = logement;
 
-    // console.log(logement);
-
-    return (
-        <main>
-            <Carousel props={pictures} />
-            <div>
-                <div className="logement__description--left">
-                    <div>{title}</div>
-                    <div>{location}</div>
-                    <Tags keywords={tags} />
-                </div>
-                <div className="logement__description-right">
-                    <div>
-                        <span>{host.name}</span>
-                        <img src={host.picture} />
+        return (
+            <main>
+                <Carousel props={pictures} />
+                <div>
+                    <div className="logement__description--left">
+                        <div>{title}</div>
+                        <div>{location}</div>
+                        <Tags keywords={tags} />
                     </div>
-                    <div>{rating}</div>
+                    <div className="logement__description-right">
+                        <div>
+                            <span>{host.name}</span>
+                            <img src={host.picture} />
+                        </div>
+                        <div>{rating}</div>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <Collapse
-                    collapseTitle="Description"
-                    collapseContent={description}
-                />
-                <Collapse
-                    collapseTitle="Équipements"
-                    collapseContent={equipments}
-                />
-            </div>
-        </main>
-    );
+                <div>
+                    <Collapse
+                        collapseTitle="Description"
+                        collapseContent={description}
+                    />
+                    <Collapse
+                        collapseTitle="Équipements"
+                        collapseContent={equipments}
+                    />
+                </div>
+            </main>
+        );
+    } catch {
+        return <Navigate to="/notFound" />;
+    }
 };
 
 export default Logement;
