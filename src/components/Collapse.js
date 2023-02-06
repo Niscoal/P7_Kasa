366d5d6b1b97bfd82674b09d4390b132875296ef
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-02-06 12:12:41 +0100

    Collapse : UseState default set to true. Logic inverted to avoid misundestanding

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -3,20 +3,21 @@ import chevronDown from "../assets/collapse_chevronDown.png";
 import chevronUp from "../assets/collapse_chevronUp.png";
 
 const Collapse = (props) => {
-    const [initial, setOpening] = useState(false); //initialize collapse state to close
-    // for undestanding ln14 & 16 : useState is set to "false", so the default (and collapse close state) is : !initial
+    const [initial, setOpening] = useState(true); //initialize collapse state to close
+    // for undestanding ln14 & 16 : useState is set to "true", so the default (and collapse close state) is : initial
 
     return (
         <div className={props.collapseSize}>
             <div onClick={() => setOpening(!initial)} className="collapseUp">
                 {/* Switch the state : false or true */}
                 <span className="collapse__title">{props.collapseTitle}</span>
-                {initial && <img src={chevronUp} alt="chevron up" />}
-                {/* initial is true when collapse is open */}
-                {!initial && <img src={chevronDown} alt="chevron down" />}
+                {/* conditional display */}
+                {initial && <img src={chevronDown} alt="chevron down" />}
+                {/* initial is false when collapse is open */}
+                {!initial && <img src={chevronUp} alt="chevron up" />}
             </div>
 
-            {initial && ( // when collapse is open, display content
+            {!initial && ( // when collapse is open, display content
                 <div className="collapseDown">
                     {Array.isArray(props.collapseContent) ? (
                         // content may be different depending on the source of the data
